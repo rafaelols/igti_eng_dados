884ec763652c65eadc180455c4000f9da92dd0fc
rafaelols <rafaelols94@gmail.com>
2020-11-22 15:19:55 -0300

    v1

--- Projeto_twitter/get_tweets.py
+++ Projeto_twitter/get_tweets.py
@@ -1,5 +1,6 @@
 import json
-from tweepy import OAuthHandler
+from tweepy import OAuthHandler, Stream, StreamListener
+from datetime import datetime
 
 # Get access variables
 with open('./vaccess_keys.txt') as access_file:
@@ -12,4 +13,25 @@ access_token = vaccess['access_token']
 access_token_secret = vaccess['access_token_secret']
 
 # Arquivo de saída para armazenar os tweets coletados
+data_atual = datetime.now.strftime("%Y-%m-%d-%H-%M-%S")
+out = open(f"collected_tweets{data_atual}.txt", "w")
 
+# Implementar uma classe para conexão com o Twitter
+class MyListener(StreamListener):
+    def on_data(self, data):
+        print(data)
+        itemString = json.dumps(data)
+        out.write(itemString + "\n")
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+# Implementar main
+if __name__ == "__main__":
+    l = MyListener()
+    auth= OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    stream = Stream(auth, l)
+    stream.filter(track=["Trump"])
